2aad809773bc7c4871076bacd1a5baee9dcd2387
pinky-banik <pinky.rani.banik@gmail.com>
2024-08-20 21:37:33 +0600

    count chart

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,27 +1,42 @@
-import UserCard from '@/components/UserCard';
-import React from 'react';
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 const AdminPage = () => {
-    return (
-        <div className='p-4 flex gap-4 flex-col md:flex-row'>
-            {/* Left */}
-            <div className='w-full lg:w-2/3'>
-                {/* UserCards */}
-                <div className='flex gap-4 flex-wrap'>
-                    <UserCard type="student"/>
-                    <UserCard type="teacher"/>
-                    <UserCard type="parent"/>
-                    <UserCard type="staff"/>
-                </div>
-            </div>
-
-
-            {/* Right */}
-            <div className='w-full lg:w-1/3'>
-                
-            </div>
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
         </div>
-    );
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            {/* <AttendanceChart /> */}
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          {/* <FinanceChart /> */}
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        {/* <EventCalendar />
+        <Announcements/> */}
+      </div>
+    </div>
+  );
 };
 
 export default AdminPage;
