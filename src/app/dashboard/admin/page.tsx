f366675792caa5a2aa1af8850b8b6e8c52f8cb66
pinky-banik <pinky.rani.banik@gmail.com>
2024-08-20 20:47:01 +0600

    admin page

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,9 +1,25 @@
+import UserCard from '@/components/UserCard';
 import React from 'react';
 
 const AdminPage = () => {
     return (
-        <div>
-            admin
+        <div className='p-4 flex gap-4 flex-col md:flex-row'>
+            {/* Left */}
+            <div className='w-full lg:w-2/3'>
+                {/* UserCards */}
+                <div className='flex gap-4 flex-wrap'>
+                    <UserCard type="student"/>
+                    <UserCard type="teacher"/>
+                    <UserCard type="parent"/>
+                    <UserCard type="staff"/>
+                </div>
+            </div>
+
+
+            {/* Right */}
+            <div className='w-full lg:w-1/3'>
+                
+            </div>
         </div>
     );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        dashSky: "#C3EBFA",
+        dashSkyLight: "#EDF9FD",
+        dashPurple: "#CFCEFF",
+        dashPurpleLight: "#F1F0FF",
+        dashYellow: "#FAE27C",
+        dashYellowLight: "#FEFCE8",
+      }
     },
   },
   plugins: [],
